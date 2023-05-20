0462ea0cade992a26dd4b0a2b3acb817b80b8ceb
zubayer22 <zubayerkh@gmail.com>
2023-05-20 14:23:19 +0600

    Edit functionality added in my toy page

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -5,19 +5,20 @@ import Swal from 'sweetalert2';
 
 const MyToys = () => {
 
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const [myToysList, setMyToysList] = useState([]);
 
 
+
     const url = `http://localhost:3000/products?email=${user?.email}`;
-    useEffect(()=> {
+    useEffect(() => {
         fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            setMyToysList(data);
-            console.log(data)
-        })
-    },[user?.email])
+            .then(res => res.json())
+            .then(data => {
+                setMyToysList(data);
+                console.log(data)
+            })
+    }, [user?.email])
 
     //Delete any toy that
     const handleDelete = id => {
@@ -54,52 +55,123 @@ const MyToys = () => {
     }
 
     //update toy details
-    const handleUpdateToyDetails = id => {
-        fetch(`http://localhost:3000/products/${id}`,{
-            method: 'PATCH',
+    // const handleUpdate = id => {
+    //     fetch(`http://localhost:3000/products/${id}`, {
+    //         method: 'PATCH',
+    //         headers: {
+    //             'content-type': 'application/json'
+    //         },
+    //         body: JSON.stringify()
+    //     })
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             if (data.modifiedCount > 0) {
+
+    //             }
+    //         })
+    // }
+
+    // Update toy details
+    // const handleUpdate = id => {
+    //     // Find the specific toy in the myToysList using the id
+    //     const updateToyDetails = myToysList.find(toy => toy._id === id);
+    //     console.log(updateToyDetails)
+
+
+    //     // Check if the toy is found
+    //     if (updateToyDetails) {
+
+    //         fetch(`http://localhost:3000/products/${id}`, {
+    //             method: 'PUT',
+    //             headers: {
+    //                 'content-type': 'application/json'
+    //             },
+    //             body: JSON.stringify({
+    //                 toy_name: updateToyDetails.toy_name,
+    //                 available_quantity: updateToyDetails.quantity,
+    //                 price: updateToyDetails.price,
+    //                 picture_url: updateToyDetails.photo,
+    //                 description: updateToyDetails.description
+    //             })
+    //         })
+    //             .then(res => res.json())
+    //             .then(data => {
+    //                 console.log(data);
+    //                 if (data.modifiedCount > 0) {
+    //                     // Handle the successful update
+    //                     Swal.fire(
+    //                         'Updated!',
+    //                         'Your Toy has been updated.',
+    //                         'success'
+    //                     );
+    //                 }
+    //             });
+    //     }
+    // };
+
+    const handleUpdate = async (updatedToy) => {
+        const id = updatedToy._id;
+
+        // ...
+
+        fetch(`http://localhost:3000/products/${id}`, {
+            method: 'PUT',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify()
+            body: JSON.stringify({
+                toy_name: updatedToy.toy_name,
+                available_quantity: updatedToy.quantity,
+                price: updatedToy.price,
+                picture_url: updatedToy.photo,
+                description: updatedToy.description
+            })
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            if(data.modifiedCount > 0) {
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    // Handle the successful update
+                    Swal.fire(
+                        'Updated!',
+                        'Your Toy has been updated.',
+                        'success'
+                    );
+                }
+            });
+    };
 
-            }
-        })
-    }
 
 
     return (
         <div>
             <div className='container mx-auto py-10'>
-            <h1 className="text-4xl font-bold text-center mb-10">My Toys</h1>
-            <div className="overflow-x-auto w-full">
-                <table className="table w-full">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>Toy Name</th>
-                            <th>Toy Image</th>
-                            <th>Category</th>
-                            <th>Price</th>
-                            <th>Quantity</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                <h1 className="text-4xl font-bold text-center mb-10">My Toys</h1>
+                <div className="overflow-x-auto w-full">
+                    <table className="table w-full">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>Toy Name</th>
+                                <th>Toy Image</th>
+                                <th>Category</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                        {
-                            myToysList.map(myToy => <SingleMyToy key={myToy._id} myToy={myToy} handleDelete={handleDelete}></SingleMyToy>)
-                        }
+                            {
+                                myToysList.map(myToy => <SingleMyToy key={myToy._id} myToy={myToy} handleDelete={handleDelete} handleUpdate={handleUpdate}></SingleMyToy>)
+                            }
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
--- src/pages/MyToys/SingleMyToy.jsx
+++ src/pages/MyToys/SingleMyToy.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaUserEdit } from 'react-icons/fa';
+import { AuthContext } from '../../providers/AuthProvider';
 
-const SingleMyToy = ({ myToy, handleDelete }) => {
-    
+const SingleMyToy = ({ myToy, handleDelete, handleUpdate }) => {
+
     const { _id, name, category, price, seller_name, available_quantity, description, picture_url } = myToy;
 
-    
+    const { user } = useContext(AuthContext);
+
+    // const handleEdit = (event) => {
+    //     event.preventDefault();
+    //     handleUpdate(_id);
+    //   };
+
+    const handleEdit = async (event) => {
+        event.preventDefault();
+      
+        const form = event.target; // Get the form element
+        const formData = new FormData(form); // Create a new FormData object
+      
+        // Convert the FormData object to a plain object
+        const updatedToy = Array.from(formData.entries()).reduce(
+          (acc, [key, value]) => ({ ...acc, [key]: value }),
+          {}
+        );
+      
+        // Pass the _id separately from the form data
+        updatedToy._id = myToy._id;
+      
+        await handleUpdate(updatedToy);
+      };
 
     return (
+
         <tr>
             <td>
                 <h3 className='font-bold'>{name}</h3>
@@ -25,16 +50,92 @@ const SingleMyToy = ({ myToy, handleDelete }) => {
                 {available_quantity}
             </td>
             <th>
-                <button className="btn btn-circle">
+                <label htmlFor={`my-modal-${_id}`} className="btn btn-circle">
                     <FaUserEdit className="h-6 w-6"></FaUserEdit>
-                </button>
+                </label>
+                {/* Put this part before </body> tag */}
+                <input type="checkbox" id={`my-modal-${_id}`} className="modal-toggle" />
+                <div className="modal">
+                    <div className="modal-box w-11/12 max-w-5xl">
+                        <form onSubmit={(event) => handleEdit(event)} className='w-3/4 mx-auto'>
+                            <div className='flex-column md:flex gap-5'>
+                                <div className="form-control w-full md:w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Your Name</span>
+                                    </label>
+                                    <input type="text" name='seller_name' placeholder="Enter your Name" className="input input-bordered" defaultValue={user?.displayName} required />
+                                </div>
+                                <div className="form-control w-full md:w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Your Email</span>
+                                    </label>
+                                    <input type="email" name='seller_email' placeholder="Enter your email" className="input input-bordered" defaultValue={user?.email} required />
+                                </div>
+                            </div>
+
+                            <div className='flex-column md:flex gap-5'>
+                                <div className="form-control w-full md:w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Toy Name</span>
+                                    </label>
+                                    <input type="text" name='toy_name' placeholder="Enter Toy name" className="input input-bordered" defaultValue={name} required />
+                                </div>
+                                <div className="form-control w-full md:w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Price</span>
+                                    </label>
+                                    <input type="number" name='price' placeholder="Enter Toy Price" className="input input-bordered" step="any" required defaultValue={price}/>
+                                </div>
+                            </div>
+
+                            
+
+                            <div className='flex-column md:flex gap-5'>
+                                <div className="form-control w-full md:w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Available Quantity</span>
+                                    </label>
+                                    <input type="number" name='quantity' placeholder="Quantity" className="input input-bordered" required defaultValue={available_quantity}/>
+                                </div>
+                                <div className="form-control w-full md:w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Toy Photo URL</span>
+                                    </label>
+                                    <input type="text" name='photo' placeholder="Enter your Toy photo url" className="input input-bordered" defaultValue={picture_url} />
+                                </div>
+                            </div>
+
+
+                            <div className="form-control w-full">
+                                <label className="label">
+                                    <span className="label-text">Toy Description</span>
+                                </label>
+                                <textarea
+                                    name="description"
+                                    placeholder="Enter Description"
+                                    className="textarea textarea-bordered h-24"
+                                    defaultValue={description}
+                                    required
+                                ></textarea>
+                            </div>
+
+                            <div className="form-control mt-6">
+                                <input className="bg-gradient-to-r from-sky-500 to-sky-700 text-white px-6 py-3 rounded font-bold text-lg" type="submit" value="Update Toy" />
+                            </div>
+                        </form>
+                        <div className="modal-action">
+                            <label htmlFor={`my-modal-${_id}`} className="btn">Close</label>
+                        </div>
+                    </div>
+                </div>
             </th>
             <th>
-                <button onClick={()=>handleDelete(_id)} className="btn btn-circle border-none bg-red-600 hover:bg-red-600">
+                <button onClick={() => handleDelete(_id)} className="btn btn-circle border-none bg-red-600 hover:bg-red-600">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
             </th>
         </tr>
+
     );
 };
 
